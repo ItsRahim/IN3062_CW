1e20af0d17ada898b049ed2bcffb20aefb108f5a
ItsRahim <rahim1605@gmail.com>
2021-11-11 16:21:16 +0000

    update

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from visualise import graph
 from sklearn.linear_model import LinearRegression
+from matplotlib import pyplot as plt
 
 df = pd.read_csv("stroke.csv")
 graph(df)
@@ -24,7 +25,8 @@ df = df.drop(df[df.smoking_status == 'Unknown'].index)
 
 df['gender'] = df['gender'].replace({'Male': 0, 'Female': 1})
 df['ever_married'] = df['ever_married'].replace({'No': 0, 'Yes': 1})
-
+df['smoking_status'] = df['smoking_status'].replace(
+    {'formerly smoked': -1, 'smokes': 1, 'never smoked': 0})
 # converting df to csv to check if data is clean
 df.to_csv('stroke_clean_data.csv', encoding='utf-8', index=False)
 
@@ -38,14 +40,17 @@ X = df[['gender', 'age', 'hypertension',
         'heart_disease', 'ever_married', 'avg_glucose_level', 'bmi', 'smoking_status']]
 y = df['stroke']
 
-#splitting teh dataset into tarining and testing data
+# splitting teh dataset into tarining and testing data
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, train_size=1/3, random_state=42)
 
-#training the algorithm
-linearModel = LinearRegression()  
-linearModel.fit(X_train, y_train) 
+# training the algorithm
+linearModel = LinearRegression()
+linearModel.fit(X_train, y_train)
 y_pred = linearModel.predict(X_test)
 
-print(pd.DataFrame({'Actual': y_test.flatten(), 'Predicted': y_pred.flatten()}))
+accuracyData = (pd.DataFrame(
+    {'Actual': y_test, 'Predicted': y_pred}))
 
+accuracyData.to_csv("stroke_accuracy.csv", encoding='utf-8', index=False)
+print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
